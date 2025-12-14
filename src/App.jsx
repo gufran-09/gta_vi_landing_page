1e17656916c6d7566ac94085b42cd8a5be67d4fb
Gufran Ahmed <gufranahmed0921@gmail.com>
2025-12-14 19:39:07 +0530

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import gsap from "gsap"
 import { ScrollTrigger,  } from "gsap/all"
+import Navbar from "./sections/Navbar"
 
 gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
-      <h1 className="text-pink">
-        Welcome to the World of GTA and GSAP
-      </h1>
+      <Navbar/>
     </main>
   )
 }
